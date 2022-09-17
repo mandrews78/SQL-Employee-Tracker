e8baf24f1718ecc205d40969bf885fc9bdf72f95
Michael Andrews <michael.andrews@pega.com>
2022-09-17 02:21:55 -0400

    created server.js file

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+require('dotenv').config()
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        //Your MySQL username
+        user: process.env.DB_USER,
+        //Your MySQL password
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
+    },
+    console.log("Connected to the company database.")
+);
+
+module.exports = db;
