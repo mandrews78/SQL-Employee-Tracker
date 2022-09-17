9f94c9a76d1a2bd165f60fc8e91e9afbb0c63cd8
Michael Andrews <michael.andrews@pega.com>
2022-09-16 21:16:28 -0400

    created Employees table in sql

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,13 @@ CREATE TABLE roles (
     CONSTRAINT fk_department FOREIGN kEY (department_id)
     REFERENCES departments(id) ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+); 
